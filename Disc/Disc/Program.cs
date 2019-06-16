2e65bef1aa47e68379da2bb7dc9d9f5b60cb1b51
4190 <lukdab@gmx.es>
2019-06-16 16:59:29 +0200

    initial project version

---
+++ Disc/Disc/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace Disc
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+        
+
+
+        private async Task MainAsync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(),null);
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = "NTMzNjg4MjcwNzE5Mjg3Mjk2.XQY7OA.DrFAb0oT2HD43l7Mw3ynA621bSo";
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1); 
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Piece of shit", "https://www.google.com/");
+        }
+
+        private Task Client_MessageReceived(SocketMessage arg)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
