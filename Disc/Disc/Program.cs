4b83accc8ae61329ccbd23a96a1516885bf9e282
4190 <lukdab@gmx.es>
2019-06-16 17:31:05 +0200

    koment

--- Disc/Disc/Program.cs
+++ Disc/Disc/Program.cs
@@ -44,7 +44,7 @@ namespace Disc
 
             await Task.Delay(-1); 
         }
-        //koment
+        //komentaaaa
         private async Task Client_Log(LogMessage Message)
         {
             Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
